ef62b0fa6d9b7165284224c6ae578c02e7fc2063
Sam Clifford <s.clifford@zengenti.com>
2021-11-16 09:04:01 +0000

    feat: cleanup example component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,41 +1,33 @@
-interface ExampleProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
+export interface ExampleProps {
+  // Is this the principal call to action on the page?
   primary?: boolean;
-  /**
-   * What background color to use
-   */
+  // What background color to use
   backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
+  // How large should the button be?
+  size?: "small" | "medium" | "large";
+  // Button contents
   label: string;
-  /**
-   * Optional click handler
-   */
+  // Optional click handler
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
+// Primary UI component for user interaction
 export const Button = ({
   primary = false,
-  size = 'medium',
+  size = "medium",
   backgroundColor,
   label,
   ...props
 }: ExampleProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
       style={{ backgroundColor }}
       {...props}
     >
