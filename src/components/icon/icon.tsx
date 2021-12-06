a4207d58259e9250196194dd25e3211bcce9a7ec
Derry Coffey <d.coffey@zengenti.com>
2021-12-06 16:12:19 +0000

    feat: Icon uses Context so that consuming app can define Icons

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,61 +1,15 @@
-//page furniture icons
-import Close from "./assets/close";
-import Collapse from "./assets/collapse";
-import Email from "./assets/email";
-import Expand from "./assets/expand";
-import Information from "./assets/information";
-import Question from "./assets/question";
-import Warning from "./assets/warning";
+import React, { createContext, useContext } from "react";
 
-// social icons
-import Facebook from "./assets/facebook";
-import FacebookRev from "./assets/facebookRev";
-import Instagram from "./assets/instagram";
-import Linkedin from "./assets/linkedin";
-import LinkedinRev from "./assets/linkedinRev";
-import Tiktok from "./assets/tiktok";
-import Twitter from "./assets/twitter";
-import YouTube from "./assets/youtube";
-
-const iconDictionary = {
-  close: <Close />,
-  collapse: <Collapse />,
-  email: <Email />,
-  expand: <Expand />,
-  facebook: <Facebook />,
-  facebookRev: <FacebookRev />,
-  information: <Information />,
-  instagram: <Instagram />,
-  linkedin: <Linkedin />,
-  linkedinRev: <LinkedinRev />,
-  question: <Question />,
-  tiktok: <Tiktok />,
-  twitter: <Twitter />,
-  warning: <Warning />,
-  youtube: <YouTube />,
-};
+export const IconDictionary = createContext<{
+  [key: string]: React.ReactChild;
+}>({});
 
 export interface IconProps {
   fill?: string;
   width?: number;
   height?: number;
   viewBox?: string;
-  type?:
-    | "close"
-    | "collapse"
-    | "email"
-    | "expand"
-    | "facebook"
-    | "facebookRev"
-    | "information"
-    | "instagram"
-    | "linkedin"
-    | "linkedinRev"
-    | "question"
-    | "tiktok"
-    | "twitter"
-    | "warning"
-    | "youtube";
+  type?: string;
 }
 
 export const Icon = ({
@@ -66,6 +20,7 @@ export const Icon = ({
   type,
   ...props
 }: IconProps) => {
+  const iconDictionary = useContext(IconDictionary);
   if (!type || !iconDictionary[type]) return null;
   return (
     <svg
--- src/stories/icon/icon.stories.tsx
+++ src/stories/icon/icon.stories.tsx
@@ -1,22 +1,67 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Icon, IconDictionary } from "../../index";
 
-import { Icon } from "../../index";
+//page furniture icons
+import Close from "./assets/close";
+import Collapse from "./assets/collapse";
+import Email from "./assets/email";
+import Expand from "./assets/expand";
+import Information from "./assets/information";
+import Question from "./assets/question";
+import Warning from "./assets/warning";
+
+// social icons
+import Facebook from "./assets/facebook";
+import FacebookRev from "./assets/facebookRev";
+import Instagram from "./assets/instagram";
+import Linkedin from "./assets/linkedin";
+import LinkedinRev from "./assets/linkedinRev";
+import Tiktok from "./assets/tiktok";
+import Twitter from "./assets/twitter";
+import YouTube from "./assets/youtube";
+
+const iconDictionary = {
+  close: <Close />,
+  collapse: <Collapse />,
+  email: <Email />,
+  expand: <Expand />,
+  facebook: <Facebook />,
+  facebookRev: <FacebookRev />,
+  information: <Information />,
+  instagram: <Instagram />,
+  linkedin: <Linkedin />,
+  linkedinRev: <LinkedinRev />,
+  question: <Question />,
+  tiktok: <Tiktok />,
+  twitter: <Twitter />,
+  warning: <Warning />,
+  youtube: <YouTube />,
+};
 
 export default {
   title: "Components/Icons",
   component: Icon,
   argTypes: {
     fill: { control: "color" },
+    type: { control: { type: "select", options: Object.keys(iconDictionary) } },
   },
 } as ComponentMeta<typeof Icon>;
 
 const Template: ComponentStory<typeof Icon> = (args) => <Icon {...args} />;
 
 export const IconSelector = Template.bind({});
+IconSelector.decorators = [
+  (story) => (
+    <IconDictionary.Provider value={iconDictionary}>
+      {story()}
+    </IconDictionary.Provider>
+  ),
+];
 
 IconSelector.args = {
   height: 24,
   width: 24,
   viewBox: "0 0 24 24",
+  fill: "#f0f",
   type: "twitter",
 };
